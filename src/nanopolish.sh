a5bb790a4eeffea63a1be01dc08bf55e4f2d1bb0
Laura White <laura.white+github@gmail.com>
2022-12-18 15:53:35 -0700

    Update nanopolish.sh

--- src/nanopolish.sh
+++ src/nanopolish.sh
@@ -1,1 +1,27 @@
-# sample Nanopolish script for generating intermediate files
+#BSUB -J nanopolish 
+#BSUB -o logs/nanopolish.%J.out
+#BSUB -e logs/nanopolish.%J.err
+#BSUB -n 1
+#BSUB -R "select[mem>100] rusage[mem=100] span[hosts=1]"
+#BSUB -q rna
+
+set -o nounset -o pipefail -o errexit -x
+
+# Sample Nanopolish script for generating intermediate files on cluster
+
+# set conda environment
+#conda activate nanopolish
+
+# fix path to plugin
+export HDF5_PLUGIN_PATH=/beevol/home/whitel/src/nanopolish/ont-vbz-hdf-plugin-1.0.1-Linux/usr/local/hdf5/lib/plugin
+
+# build an index
+nanopolish index -d fast5_pass_singles -s out/sequencing_summary.txt allreads.fastq
+
+# combine signal and sequence data
+# is this picky about having the reference locally? can't remember, just going to do it.
+nanopolish eventalign \
+    --reads allreads.fastq \
+    --bam allreadsT.bwa.sc1splint.bam \
+    --genome splint.fa \
+    --scale-events > eventalign_output.txt
